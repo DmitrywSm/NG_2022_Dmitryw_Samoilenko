bcc6e9065d9b5c322b91c64ee6cba718562fd323
Dmitryw Samoilenko <dmitrywsamoilenko16@gmail.com>
2022-10-15 23:05:31 +0300

    modified test file

---
+++ test.py
@@ -0,0 +1,6 @@
+numberOne = int(input("number 1: "))
+numberTwo = int(input("number 2: "))
+sum = numberOne + numberTwo
+if sum != 3: 
+    print("sum is not equal 3")
+else: print("sum is equal to 3")

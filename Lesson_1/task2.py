277c535bbaa294cdcdcc7d445a58fb155e411b4c
Dmitryw Samoilenko <dmitrywsamoilenko16@gmail.com>
2022-10-18 18:12:44 +0300

    added task2

---
+++ Lesson_1/task2.py
@@ -0,0 +1,42 @@
+
+
+from os import spawnv
+
+
+numberOne = float(input("Input first number: "))
+operator = str(input("Input math operator (*,/,+,-,sqrt,pow): "))
+numberTwo = float(input("Input second number: "))
+result = float()
+spaceLine = "====================================="
+
+print(spaceLine)
+
+if operator == "*": 
+    result = numberOne * numberTwo
+elif operator == "/":
+    result = numberOne / numberTwo
+elif operator == "+":
+    result = numberOne + numberTwo
+elif operator == "-":
+    result = numberOne - numberTwo
+elif operator == "sqrt":
+    if numberTwo != 0 :
+        result = numberOne ** (1/numberTwo)
+    else:result = str(float)
+    result = "infinity"
+    print(numberOne, operator, numberTwo, "=", result )
+    print(spaceLine)
+    print("")
+    input("Press Enter to close the program")
+    exit()
+elif operator == "pow":
+    result = numberOne ** numberTwo
+    
+result = round(result, 3)
+
+print(numberOne, operator, numberTwo, "=", result )
+print(spaceLine)
+print("")
+input("Press Enter to close the program")
+
+exit()
---
+++ task3.py
@@ -0,0 +1,8 @@
+from datetime import date, datetime
+from sqlite3 import Timestamp
+import time
+datetime.fromisoformat('2011-11-04')
+datetime.fromtimestamp(time.time())
+Uhour = str(datetime.hour)
+print(Uhour)
+exit()

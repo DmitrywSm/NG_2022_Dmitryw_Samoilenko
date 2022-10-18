077ac9c860f53ec741073d804d98f5087800d296
Dmitryw Samoilenko <dmitrywsamoilenko16@gmail.com>
2022-10-18 19:05:06 +0300

    added task3

---
+++ Lesson_1/task3.py
@@ -0,0 +1,8 @@
+from os import curdir
+import datetime
+import time
+CurrentFormat = time.strftime("day: %d\nhour: %H\nminute: %M\nSecond: %S")
+input("Press Enter to know current time")
+print(CurrentFormat)
+input("Press Enter to close the program")
+exit()

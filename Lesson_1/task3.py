b86943174d7a07ec1d88e65c87bd1206f1b86580
Dmitryw Samoilenko <dmitrywsamoilenko16@gmail.com>
2022-10-18 21:31:13 +0300

    modified task3

--- Lesson_1/task3.py
+++ Lesson_1/task3.py
@@ -1,11 +1,10 @@
-from email.utils import localtime
-from os import curdir
 import datetime
-import time
-localtime = 555
-CurrentTime = input("Enter required number of seconds: ")
-CurrentFormat = time.strftime("day: %d\nhour: %H\nminute: %M\nSecond: %S")
 
-print(CurrentFormat)
+currentTime = float(input("Enter required number of seconds: "))
+
+currentUnixTime = datetime.datetime.fromtimestamp(currentTime, tz=datetime.timezone.utc)
+currentTimeFormatDay = currentUnixTime.strftime("day: %d\nhour: %H\nminute: %M\nsecond: %S")
+print(currentTimeFormatDay)
+print(currentUnixTime)
 input("Press Enter to close the program")
 exit()

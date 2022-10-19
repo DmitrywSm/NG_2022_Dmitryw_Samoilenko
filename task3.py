d4bebed544d7ae67eb16fe01788493a7854d7f1d
Dmitryw Samoilenko <dmitrywsamoilenko16@gmail.com>
2022-10-19 15:49:01 +0300

    removed unnessesary file

--- task3.py
+++
@@ -1,8 +0,0 @@
-from datetime import date, datetime
-from sqlite3 import Timestamp
-import time
-datetime.fromisoformat('2011-11-04')
-datetime.fromtimestamp(time.time())
-Uhour = str(datetime.hour)
-print(Uhour)
-exit()

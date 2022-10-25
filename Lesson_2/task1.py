f2fe451ee46b0e31833415ef30336a59ec276179
Dmitryw Samoilenko <dmitrywsamoilenko16@gmail.com>
2022-10-25 22:26:33 +0300

    added task1, second lesson folder

---
+++ Lesson_2/task1.py
@@ -0,0 +1,16 @@
+phraseList = str(input("Enter your phrase:"))
+phraseList = phraseList.lower()
+phraseCount = {}
+
+#makes new key for every unique element and gives values from number of every unique element
+for i in  phraseList: 
+    phraseCount [i] = phraseList.count(i)    
+
+#sorts keys by values 
+sortedCount = sorted(phraseCount, key=phraseCount.get, reverse = True)
+
+#for every key prints key+value from sorted dictionary
+for perKey in sortedCount:
+    print(str(perKey), ": ", str(phraseCount.get(perKey)))
+
+exit()

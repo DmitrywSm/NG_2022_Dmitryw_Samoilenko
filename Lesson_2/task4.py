dc55535d5eb21e33db15f23c40567e99ab3eab8b
Dmitryw Samoilenko <dmitrywsamoilenko16@gmail.com>
2022-10-26 10:03:42 +0300

    added task4

---
+++ Lesson_2/task4.py
@@ -0,0 +1,17 @@
+factSize = int(input("input number to get factorial: "))
+userFactSize = factSize
+factList = []
+multip = 1
+
+while factSize > 0 :
+    factList.append(factSize)
+    factSize = factSize - 1 
+
+for i in factList:
+    print(multip, "* ", i)
+    multip = multip * i
+print("====================================")
+print( userFactSize,"!" ," = ",multip)
+input("press enter to close app")
+
+exit()

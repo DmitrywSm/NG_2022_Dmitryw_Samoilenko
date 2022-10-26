ad2aa79fe3caa860a9bce26b3b7ea0ae17f5581f
Dmitryw Samoilenko <dmitrywsamoilenko16@gmail.com>
2022-10-26 16:34:10 +0300

    fixed sorted method

--- Lesson_2/task5.py
+++ Lesson_2/task5.py
@@ -1,16 +1,21 @@
-userStr = str(input("Enter your list (1, 2, ...) "))
+
+userStr = (input("Enter your list (1, 2, ...) "))
 userList = userStr.split(sep=", ")
-userList.sort(reverse= True)
 sum = 0
 
-for i in userList[1:int(len(userList)-1)]:
+for i in range(0, len(userList)):
+    userList[i]= int(userList[i])
+
+sortedList = sorted(userList, reverse= True)
+
+for i in sortedList[1:int(len(sortedList)-1)]:
     sum = sum + int(i) 
 
 print("=============================================")
-print("max: ", userList[0])
-print("min: ", userList[(int(len(userList))-1)])
-print(f"sum({userList[1]}:{userList[int(len(userList)-2)]}) = ", sum)
-print(str(userList))
+print("max: ", sortedList[0])
+print("min: ", sortedList[(int(len(sortedList))-1)])
+print(f"sum({sortedList[1]}:{sortedList[int(len(sortedList)-2)]}) = ", sum)
+print(str(sortedList))
 
 input("press enter to close app")
 exit()

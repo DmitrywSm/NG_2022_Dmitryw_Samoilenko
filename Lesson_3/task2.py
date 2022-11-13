71c74c27568c6567fe7350aef1f8b024b8ae4c55
Dmitryw Samoilenko <dmitrywsamoilenko16@gmail.com>
2022-11-13 16:32:30 +0200

    added task2

---
+++ Lesson_3/task2.py
@@ -0,0 +1,88 @@
+
+def askString ():
+    userString = input("Enter your text: ")
+    return userString
+
+optionList = ["Sort text", "Count number of elements", "Output only vowels", "Output only consonants", "Break by words and print words from the end", "Print word by number", "Enter the line again", "Exit"]
+
+# sorts user text (doesn't print)
+def sortText (userString):
+    userString = list(userString)
+    sortedList = sorted(userString, reverse=False)
+    print(''.join(sortedList))
+
+#print numbers of elements in list
+def countNumber(userString):
+    print(len(userString))
+
+def onlyVowels(userString):
+    vowelsSet = {'a', 'i', 'e', 'o', 'u', 'y'}
+    for letter in userString.lower():
+        if letter in vowelsSet:
+            print(letter, end="")
+    print('')
+def onlyConsonants (userString):
+    consonantsSet = { 'b', 'c', 'd', 'f', 'g', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 's', 't', 'v', 'x', 'z', 'h', 'r', 'w', 'y'} 
+    for letter in userString.lower():
+        if letter in consonantsSet:
+            print(letter, end="")
+    print('')
+#print last two words in line
+def breakAndPrint (userString):
+    wordList = breakByWord(userString)
+    print(sorted(wordList, reverse = True))
+
+def breakByWord (userString):
+    return userString.split(sep =" ")
+
+def wordByNumber (userString):
+    wordList = breakByWord(userString)
+    try: 
+        userIndex = int(input("Enter index of word: "))
+        print(wordList[userIndex])
+    except: print("unexpected index")
+
+def writeLine (userString):
+    print(userString)
+
+def exitFunc(userString): 
+    return 1
+
+functionDict = {
+    "0": sortText,
+    "1": countNumber,
+    "2": onlyVowels,
+    "3": onlyConsonants,
+    "4": breakAndPrint,
+    "5": wordByNumber,
+    "6": writeLine,
+    "7": exitFunc,
+}
+
+def showOptions (optionList):
+    for element in optionList :
+        print( "[",str(optionList.index(element)),"] " ,element)
+
+def askOption ():
+        userChoise = input("choose the option ")
+        return userChoise
+     
+def main (optionList):
+    userString = askString()
+    i = 0 
+    while i != 1 :
+        showOptions(optionList)
+        try: 
+            callfunc = functionDict[askOption()]
+            print("===========================")
+            a = callfunc(userString)
+            print("===========================")
+        except: 
+            print("===========================")
+            print("type another option index")
+            print("===========================")
+        if a == exitFunc(userString):
+            print("bye")
+            exit()
+
+main(optionList)
